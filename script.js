9a54afa032f5f6c57af23d3e7f41f0487cbbe362
Keagan Strydom <keagan@strysie.com>
2025-03-05 22:28:03 +0200

    Add cell creation

---
+++ script.js
@@ -0,0 +1,9 @@
+import Grid from "./Grid.js"
+
+const gameBoard = document.getElementById("grid")
+console.log(gameBoard)
+const grid = new Grid(gameBoard)
+
+console.log(grid.randomOpenCell())
+//grid.randomOpenCell().tile = new Tile(gameBoard)
+//grid.randomOpenCell().tile = new Tile(gameBoard)
